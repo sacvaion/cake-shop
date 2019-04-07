1d67a56c67c5226b74bf601f143abea2515d935f
https://github.com/sacvaion/cake-shop.git <sacvaion@gmail.com>
2019-04-06 20:43:41 -0500

    Cambio en version de compilacion

--- Pasteleria/Datos/My Project/Application.Designer.vb
+++ Pasteleria/Datos/My Project/Application.Designer.vb
@@ -32,7 +32,8 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Datos.Form1
+            Me.MainForm = Global.Datos.Cls_Conexion
+
         End Sub
     End Class
 End Namespace
